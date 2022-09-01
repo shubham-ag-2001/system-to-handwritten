ca7eb1579e2364ee74cdd59276e7752bd834cf95
Shubham Agarwal <106271248+shubham-ag-2001@users.noreply.github.com>
2022-09-02 01:09:45 +0530

    name

--- js/app.mjs
+++ js/app.mjs
@@ -18,8 +18,7 @@ import { setInkColor, toggleDrawCanvas } from './utils/draw.mjs';
  * To contribute, you can follow the imports above and make changes in the file
  * related to the issue you've choosen.
  *
- * If you have any questions related to code, you can drop them in my Twitter DM @saurabhcodes
- * or in my email at saurabhdaware99@gmail.com
+
  *
  * Thanks! and Happy coding 🌻
  *
